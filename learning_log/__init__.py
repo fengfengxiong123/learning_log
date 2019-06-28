51071a2d77eb4866ab80863ba6f7dbddac94b138
fengfengxiong123 <359975715@qq.com>
2019-06-28 11:26:31 +0800

    1

---
+++ learning_log/__init__.py
@@ -0,0 +1,4 @@
+#生产环境取消下面注释
+
+import pymysql
+pymysql.install_as_MySQLdb()
---
+++ learning_logs/serializers.py
@@ -0,0 +1,22 @@
+﻿from .models import Article,ArtContent
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+#serializers定义api的表现形式
+class ArticleSerializers(serializers.HyperlinkedModelSerializer):
+    #外键序列化
+    user_owner=serializers.CharField(source='user_owner.username')
+    user_id = serializers.IntegerField(source='user_owner.id')
+    #使用ModelSerializer序列化model模型
+    class Meta:
+    #指定序列化的模型
+        model=Article
+        fields=('art_name','art_add_date','art_author','art_type','art_status','art_introduction','art_name_used','user_owner','user_id')
+    		
+class ArtContentSerializers(serializers.HyperlinkedModelSerializer):
+    article=serializers.CharField(source='article.art_name')
+    article_id=serializers.IntegerField()
+    class Meta:
+        model=ArtContent
+        fields=('chapter_name','chapter_content','chapter_add_date','article','article_id')
+
